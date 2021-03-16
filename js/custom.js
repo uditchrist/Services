2831b7ee8bcd342c298ffa40be86c9e3db8b6e78
Udit <68955313+uditchrist@users.noreply.github.com>
2021-03-16 13:39:14 +0530

    updated on 16/03

--- js/custom.js
+++ js/custom.js
@@ -85,3 +85,12 @@
     });  
 
 })(jQuery);
+
+// $('.dropdown-toggle').click(function(e) {
+//   e.preventDefault();
+//   setTimeout($.proxy(function() {
+//     if ('ontouchstart' in document.documentElement) {
+//       $(this).siblings('.dropdown-backdrop').off().remove();
+//     }
+//   }, this), 0);
+// });
